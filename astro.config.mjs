d94a106abd47f2d7d438bb6245d02267a7ef50f7
danyneyra <danydanieln@hotmail.com>
2025-06-26 23:18:57 -0500

    feat: Implementación de GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   base: "/capacitaciones",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [partytown()]
 });
